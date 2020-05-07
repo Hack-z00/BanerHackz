31894a1632b597f9f7affa214a2a2297bec2e3b4
Hack-z00 <64979825+Hack-z00@users.noreply.github.com>
2020-05-07 12:31:42 -0500

    BanerZ.sh

---
+++ BanerZ.sh
@@ -0,0 +1,146 @@
+
+clear
+echo ""
+echo ""
+echo ""
+echo -e "$amarillo   _/<(°-')>\_===>    HACK Z  <===_/<('-°)>\_"
+echo ""
+echo ""
+echo ""
+#COLORES
+rojo='\e[1m\e[31m'
+verde='\e[1m\e[32m'
+blanco='\e[1m\e[38m'
+amarillo='\e[1m\e[33m'
+azulcielo='\e[1m\e[36m'
+   sleep 3.0
+echo -e "$rojo |==============================================|"
+   sleep 0.5
+echo -e "$verde |            Created by: HACK Z                |"
+   sleep 0.5
+echo -e "$rojo |==============================================|"
+   sleep 0.5
+echo -e "$blanco |            Script by: HACK Z                 |"
+   sleep 0.5
+echo -e "$amarillo |            YOU-TUBER: HACK Z                 |"
+   sleep 0.5
+echo -e "$blanco |----------------------------------------------|"
+   sleep 0.5
+echo -e "$azulcielo |        LIFE OF PROGRAMMER [ HACK-Z ]         |"
+   sleep 0.5
+echo -e "$blanco |----------------------------------------------|"
+   sleep 0.5
+echo -e "$rojo |==============================================|"
+   sleep 0.5
+echo -e "$verde |      SCRIPT HECHO PARA MIS SUBSCRIPTORES     |"
+   sleep 0.5
+echo -e "$rojo |==============================================|"
+   sleep 3.0
+echo -e "$verde[ESPERA UN MOMENTO]"
+   sleep 15.0
+#canal de HACK Z
+termux-open https://m.youtube.com/channel/UCZGGawu3A02OBVSYg8CTYXQ
+#clear
+#figlet -f big BANNER | lolcat
+cyan='\e[0;36m'
+lightgreen='\e[1;32m'
+red='\e[1;31m'
+yellow='\e[1;33m'
+#echo -e $lightgreen "\e[1mSI TIENES DUDAS COMUNICARME EN MI CANAL DE YOU-TUBE... "
+#echo " "
+#echo -e $yellow "\e[1mBANNER-PCH CREADOR: \e[0;31mHACK Z... "
+#echo -e "\e[0;31m CANAL DE \e[0;32mYOU-TUBE:  \e[0;34mhttps://m.youtube.com/channel/UCZGGawu3A02OBVSYg8CTYXQ"
+#echo -e "\e[0;31mH\e[0;32mA\e[0;31mC\e[0;32mK\e[0;31mI\e[0;32mN\e[0;31mG \e[0;32mP\e[0;31mC\e[0;32mH"
+
+#echo " "
+#echo " "
+echo " "
+echo -e -n "\e[1m\e[33m\nCual es tu  \e[31m\e[33mNombre\e[32m : "
+read varbanner
+echo -e -n "\e[1m\e[33m\nCual es tu palabra clave Nombre\e[32m : "
+read varcowsay
+echo  "cowsay -f eyes $varcowsay | lolcat" > C0w54y.txt
+echo "toilet -f big '    $varbanner' -F gay | lolcat" > 84nn3r.txt
+echo
+#CARPETA RAIZ
+echo "clear" > cl34r.txt
+cat "cl34r.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat "C0w54y.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+ls
+cat "84nn3r.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+#MENSAJE DE FINAL
+clear
+    sleep 2.0
+echo "GRACIAS POR USAR MI SCRIP"
+   sleep 1.0
+echo ""
+echo -e "$rojo ESTE ARCHIVO ESTA ECHO PARA USTEDES MIS $verde SUSCRIPTORES"
+   sleep 1.0
+echo -e "\e[0;33m"
+   figlet ASPIRA A LO IMPOSIBLE
+   sleep 1.0
+echo -e "\e[0;34m"
+   figlet NINGUN SISTEMA ES SEGURO
+echo ""
+echo -e "$rojo--------------------"
+   sleep 1.0
+echo "---=H============---"
+   sleep 1.0
+echo "---==A===========---"
+   sleep 1.0
+echo "---===C==========---"
+   sleep 1.0
+echo "---====K=========---"
+   sleep 1.0
+echo "---===== ========---"
+   sleep 1.0
+echo "---======Z=======---"
+   sleep 1.0
+echo -e "$rojo--------------------"
+   sleep 3.0
+clear
+echo -e "\e[1m\e[31mFAVOR \e[1m\e[32mDE \e[1m\e[31mABRIR \e[1m\e[32mOTRA \e[1m\e[33mVENTANA"
+echo -e "$azulcielo"
+   sleep 1.0
+echo "<H A C K - Z"
+   sleep 1.0
+echo "<H A C K - Z"
+   sleep 1.0
+echo "<H A C K - Z"
+   sleep 1.0
+echo "<H A C K - Z"
+   sleep 1.0
+echo "<H A C K - Z"
+   sleep 1.0
+echo "<H A C K - Z"
+   sleep 1.0
+echo "<H A C K - Z"
+   sleep 1.0
+echo "<H A C K - Z"
+   sleep 1.0
+echo "<H A C K - Z"
+   sleep 1.0
+echo "<H A C K - Z"
+   sleep 1.0
+echo "<H A C K - Z"
+   sleep 1.0
+echo "<H A C K - Z"
+   sleep 1.0
+#MUNECO
+echo -e "$rojo"
+echo "----------"
+   sleep 0.5
+echo "    --    "
+   sleep 0.5
+echo "   ----   "
+   sleep 0.5
+echo "_/<[°°]>\_"
+   sleep 0.5
+echo "   [  ]   "
+   sleep 0.5
+echo "   //\\\  "
+   sleep 0.5
+echo "  //  \\\ "
+   sleep 0.5
+echo "----------"
+echo ""
