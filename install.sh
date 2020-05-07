35d773ad41129727cfd640dce44d6f04ce332ff2
Hack-z00 <64979825+Hack-z00@users.noreply.github.com>
2020-05-07 12:28:19 -0500

    install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+clear
+apt update -y
+apt upgrade -y
+apt install figlet -y
+echo "$azulcielo"
+figlet instalando
+  sleep 3.0
+apt install toilet -y
+apt install cowsay -y
+apt install nano -y
+apt install ruby -y
+gem install lolcat
+#PRIMER MENSAJE
+figlet -f big H A C K  Z !!! | lolcat
+echo
